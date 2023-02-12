f50c90ffe48e663814a97626990258dacb0ffec2
jramsgz <47223013+jramsgz@users.noreply.github.com>
2023-02-12 19:29:25 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jramsgz/articpad
+
+go 1.20
---
+++ ui/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { RouterView, useRoute } from "vue-router";
+import NavbarLayout from "./layouts/NavbarLayout.vue";
+import FullPageLayout from "./layouts/FullPageLayout.vue";
+
+const route = useRoute();
+
+const showNavbar = computed(() => {
+  const showNavbar = route?.meta?.showNavbar;
+  if (showNavbar) {
+    return NavbarLayout;
+  }
+  return FullPageLayout;
+});
+</script>
+
+<template>
+  <div class="h-full">
+    <component :is="showNavbar">
+      <RouterView />
+    </component>
+  </div>
+</template>
