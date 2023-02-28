288f9961f9a42d292f40ba60124dbe41065ddb69
jramsgz <47223013+jramsgz@users.noreply.github.com>
2023-02-28 23:30:15 +0100

    feat: remove CSRF middleware since our JWT tokens should be already safe without it

--- middleware/middlewares.go
+++ middleware/middlewares.go
@@ -1,13 +1,9 @@
 package middleware
 
 import (
-	"time"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/csrf"
-	"github.com/gofiber/fiber/v2/utils"
 	"github.com/jramsgz/articpad/config"
 	"github.com/rs/zerolog"
 )
@@ -20,13 +16,6 @@ func RegisterMiddlewares(app *fiber.App, logger zerolog.Logger) {
 	app.Use(cors.New(cors.Config{
 		MaxAge:       1800,
 		AllowOrigins: config.Config("APP_URL", "http://localhost:3000"),
-	}))
-	app.Use(csrf.New(csrf.Config{
-		KeyLookup:      "header:X-Csrf-Token",
-		CookieName:     "articpad_csrf_",
-		CookieSameSite: "Strict",
-		Expiration:     3 * time.Hour,
-		KeyGenerator:   utils.UUID,
 	}))
 	app.Use(compress.New(compress.Config{
 		Level: compress.LevelBestSpeed, // 1
