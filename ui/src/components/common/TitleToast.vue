0c72b583ae62207a769aa9c80e00f31737134219
jramsgz <47223013+jramsgz@users.noreply.github.com>
2023-08-01 17:15:58 +0200

    feat: add custom Toast with Title

---
+++ ui/src/components/common/TitleToast.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="font-bold text-xl">{{ title }}</div>
+  <div>{{ body }}</div>
+</template>
+<script setup lang="ts">
+defineProps({
+  title: {
+    required: true,
+    type: String,
+  },
+  body: {
+    required: true,
+    type: String,
+  },
+});
+</script>
